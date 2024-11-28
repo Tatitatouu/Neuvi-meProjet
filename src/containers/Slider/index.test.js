821aed5285ee2f9bff64d9dc7ade9f6abf38edfc
Tatitatouu <tatiana68270wi@gmail.com>
2024-11-28 14:56:01 +0100

    rajout des await dans index.test.js Slider

--- src/containers/Slider/index.test.js
+++ src/containers/Slider/index.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Slider from "./index";
 import { api, DataProvider } from "../../contexts/DataContext";
 
@@ -6,7 +6,8 @@ const data = {
   focus: [
     {
       title: "World economic forum",
-      description: "Oeuvre à la coopération entre le secteur public et le privé.",
+      description:
+        "Oeuvre à la coopération entre le secteur public et le privé.",
       date: "2022-02-29T20:28:45.744Z",
       cover: "/images/evangeline-shaw-nwLTVwb7DbU-unsplash1.png",
     },
@@ -33,6 +34,11 @@ describe("When slider is created", () => {
       <DataProvider>
         <Slider />
       </DataProvider>
+    );
+    await screen.findByText("World economic forum");
+    await screen.findByText("janvier");
+    await screen.findByText(
+      "Oeuvre à la coopération entre le secteur public et le privé."
     );
   });
 });
